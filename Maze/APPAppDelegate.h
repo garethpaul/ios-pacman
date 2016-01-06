47da47fc1124613a021267cf111cfdba8c4b5b0d
Gareth Jones <gareth@garethpaul.com>
2016-01-05 20:00:00 -0600

    Add app

---
+++ Maze/APPAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  APPAppDelegate.h
+//
+
+#import <UIKit/UIKit.h>
+
+@class APPViewController;
+
+@interface APPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) APPViewController *viewController;
+
+@end
