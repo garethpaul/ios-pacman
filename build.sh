9f6bec9558800d4373ff07d6904c0bbb5317e462
Gareth Jones <gareth@garethpaul.com>
2016-10-20 20:00:00 -0600

    Add build script

---
+++ build.sh
@@ -0,0 +1,16 @@
+
+#!/bin/sh
+
+set -eu
+
+function ci_build() {
+    NAME=$1
+    xcodebuild -project "Maze.xcodeproj" \
+               -scheme "Maze" \
+               -destination "platform=iOS Simulator,name=${NAME}" \
+               -sdk iphonesimulator \
+               -configuration "Debug" \
+               build
+}
+
+ci_build "iPhone 5"
